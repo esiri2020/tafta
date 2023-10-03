175ebccfdc0366f74033ec38d9d23611c6ab7e76
basitol <basitolaitan27@gmail.com>
2023-10-03 14:32:44 +0100

    updated enrollment process

--- pages/api/enrollments.tsx
+++ pages/api/enrollments.tsx
@@ -4,6 +4,7 @@ import data from "../../input.json" assert { type: "JSON" };
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../lib/prismadb";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { Enrollment, User } from "@prisma/client";
 
 export const bigint_filter = (data: Object) => {
   return JSON.parse(
@@ -30,54 +31,27 @@ export default async function handler(
     user_email = user_email.toLowerCase();
 
     try {
-      // set enrolled to true when uncommenting this out
-      // const transaction = await prisma.$transaction(async (tx)=> {
-      //   const user = await tx.user.findUniqueOrThrow({
-      //     where: { email: user_email },
-      //     include: {
-      //       userCohort: true
-      //     }
-      //   })
-      //   const userCohort = await tx.userCohort.findUniqueOrThrow({
-      //     where:{
-      //       id: data.userCohortId
-      //     },
-      //     include:{
-      //       cohort: true
-      //     }
-      //   })
-      //   const thinkific_data = {
-      //     course_id: data.course_id,
-      //     user_id: user.thinkific_user_id,
-      //     activated_at: new Date(Date.now()).toISOString(),
-      //     expiry_date: new Date(userCohort.cohort.end_date).toISOString()
-      //   }
-      //   const response = await api.post('/enrollments', thinkific_data)
-      //   console.log(response)
-      //   if(response.status===201){
-      //     if (data.percentage_completed) {
-      //       data.percentage_completed = parseFloat(data.percentage_completed)
-      //     }
-
-      //     const enrollment = await tx.enrollment.create({
-      //       data: {
-      //         enrolled: true,
-      //         ...data,
-      //         userCohort: {
-      //           connect: {id: data.userCohortId}
-      //         }
-      //       }
-      //     })
-      //     return enrollment
-      //   }
-      //   return data
-      // })
       const user = await prisma.user.findUniqueOrThrow({
         where: { email: user_email },
         include: {
           userCohort: true,
         },
       });
+
+      const userProfile = await prisma.profile.findUnique({
+        where: {
+          userId: user.id, // Assuming userId matches the user's email
+        },
+        select: {
+          ageRange: true,
+          stateOfResidence: true,
+          educationLevel: true,
+        },
+      });
+
+      if (!userProfile) {
+        return res.status(404).send({ message: "User profile not found" });
+      }
 
       if (data.percentage_completed) {
         data.percentage_completed = parseFloat(data.percentage_completed);
@@ -92,10 +66,197 @@ export default async function handler(
           },
         },
       });
-      //Send success response
-      return res
-        .status(201)
-        .send(bigint_filter({ message: "Enrollment created", ...enrollment }));
+
+      const ageRange = userProfile.ageRange;
+      const location = userProfile.stateOfResidence?.toLowerCase();
+      const educationLevel = userProfile.educationLevel?.toLowerCase();
+
+      let minRange, maxRange;
+      maxRange = 0;
+      minRange = 0;
+
+      if (ageRange !== undefined && ageRange !== null) {
+        [minRange, maxRange] = ageRange.split("-").map(Number);
+        if (
+          !isNaN(minRange) &&
+          !isNaN(maxRange) &&
+          minRange < 16 &&
+          maxRange > 35
+        ) {
+          console.error(`User with name does not have a valid age range`);
+        }
+      } else {
+        console.error(
+          `User with name does not have a valid age range specified`
+        );
+      }
+
+      // Check location
+      const allowedLocations = ["lagos", "kano", "ogun"];
+      if (!location || !allowedLocations.includes(location)) {
+        console.error(`User with name is not in an allowed location`);
+      }
+
+      // Check level of education
+      const allowedEducationLevels = [
+        "secondary_school",
+        "college_of_education",
+        "nd_hnd",
+        "bsc",
+        "msc",
+      ];
+      if (!educationLevel || !allowedEducationLevels.includes(educationLevel)) {
+        console.error(
+          `User with name does not meet the education level requirement`
+        );
+      }
+
+      // If all requirements are met, register on LMS
+      if (
+        minRange >= 16 &&
+        maxRange <= 35 &&
+        (location === "lagos" || location === "ogun" || location === "kano") &&
+        (educationLevel === "bsc" || educationLevel === "msc")
+      ) {
+        const promises: Promise<User>[] = [];
+        const enrollment_promises: Promise<Enrollment>[] = [];
+
+        const activeEnrollment = enrollment; // Get the first enrollment
+        const course_id = `${activeEnrollment?.course_id}`;
+        const enrollmentUID = `${activeEnrollment?.uid}`;
+
+        if (user.thinkific_user_id === null) {
+          const taftaAPIData = {
+            email: user.email,
+            first_name: user.firstName,
+            last_name: user.lastName,
+            skip_custom_fields_validation: true,
+            send_welcome_email: true,
+          };
+
+          const response = await api
+            .post("/users", taftaAPIData)
+            .catch((error) => {
+              // console.log(error);
+              //   return res
+              //     .status(400)
+              //     .send({ message: error.response?.data || error.message });
+              // });
+            });
+          if (response?.status === 201) {
+            const promises: Promise<User>[] = [];
+
+            const updatedUserPromise = prisma.user.update({
+              where: {
+                id: user.id,
+              },
+              data: {
+                thinkific_user_id: `${response?.data.id}`,
+              },
+              include: {
+                profile: {
+                  select: {
+                    id: true,
+                  },
+                },
+                userCohort: {
+                  select: {
+                    enrollments: {
+                      select: {
+                        enrolled: true,
+                        course_name: true,
+                        course_id: true,
+                      },
+                    },
+                  },
+                },
+              },
+            });
+
+            // Wait for the promise to resolve
+            const updatedUser = await updatedUserPromise;
+            console.log("updatedUser: ", updatedUser);
+            promises.push(Promise.resolve(updatedUser));
+
+            const thinkific_data = {
+              course_id: course_id,
+              user_id: response.data.id,
+              activated_at: new Date(Date.now()).toISOString(),
+              // expiry_date: user?.userCohort?.pop()?.cohort?.end_date ? new Date(user?.userCohort?.pop()?.cohort?.end_date ).toISOString()
+            };
+            const response3 = await api.post("/enrollments", thinkific_data);
+            if (response3.status === 201) {
+              const { data: enrollment_data } = response3;
+              let { user_email, user_name, ...data } = enrollment_data;
+              if (data.percentage_completed) {
+                data.percentage_completed = parseFloat(
+                  data.percentage_completed
+                );
+              }
+
+              const enrollment_promises: Promise<Enrollment>[] = [];
+
+              const enrollmentPromise = prisma.enrollment.update({
+                where: {
+                  uid: enrollmentUID,
+                },
+                data: {
+                  enrolled: true,
+                  ...data,
+                },
+              });
+
+              const enrollment = await enrollmentPromise; // Use await here
+              enrollment_promises.push(Promise.resolve(enrollment));
+              console.log("enrollment : ", enrollment);
+            }
+          }
+
+          // const response2 = await api.post('/group_users', {
+          //     group_names: [user.userCohort.pop()?.cohort.name],
+          //     user_id: response.data.id
+          // })
+        } else {
+          const thinkific_data = {
+            course_id: course_id,
+            user_id: user.thinkific_user_id,
+            activated_at: new Date(Date.now()).toISOString(),
+            // expiry_date: user?.userCohort?.pop()?.cohort?.end_date ? new Date(user?.userCohort?.pop()?.cohort?.end_date ).toISOString()
+          };
+          const response3 = await api.post("/enrollments", thinkific_data);
+          if (response3.status === 201) {
+            const { data: enrollment_data } = response3;
+            let { user_email, user_name, ...data } = enrollment_data;
+            if (data.percentage_completed) {
+              data.percentage_completed = parseFloat(data.percentage_completed);
+            }
+            const enrollment = prisma.enrollment.update({
+              where: {
+                uid: enrollmentUID,
+              },
+              data: {
+                enrolled: true,
+                ...data,
+              },
+            });
+            enrollment_promises.push(enrollment);
+
+            console.log("enrollment", enrollment);
+          }
+        }
+
+        return res
+          .status(201)
+          .send(
+            bigint_filter({ message: "Enrollment created", ...enrollment })
+          );
+      } else {
+        // Send a response indicating that the user doesn't meet the requirements but has been enrolled
+        return res.status(400).send({
+          error:
+            "User does not meet one or more requirements but has been enrolled.",
+        });
+      }
     } catch (err) {
       console.error(err);
       if (err instanceof PrismaClientKnownRequestError)
@@ -108,7 +269,6 @@ export default async function handler(
 
   if (req.method === "PUT") {
     let { user_email, ...data } = JSON.parse(req.body);
-    console.log(data, user_email);
     user_email = user_email.toLowerCase();
 
     try {
@@ -233,27 +393,6 @@ export default async function handler(
               : undefined,
           ...status_object,
         },
-        // include: {
-        //   userCohort: {
-        //     select: {
-        //       user: {
-        //         select: {
-        //           id: true,
-        //           firstName: true,
-        //           lastName: true,
-        //           email: true,
-        //           profile: {
-        //             select: {
-        //               gender: true,
-        //             },
-        //           },
-        //         },
-        //       },
-        //     },
-        //   },
-        // },
-        // take,
-        // skip,
         include: {
           userCohort: {
             select: {
