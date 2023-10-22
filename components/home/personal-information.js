631dae6dd99d87eb1de36a437deb3028068baa15
Esiri Ekwale <esiri2000@gmail.com>
2023-10-22 16:42:17 +0100

    personal information edited commit

--- components/home/personal-information.js
+++ components/home/personal-information.js
@@ -27,6 +27,8 @@ const nigeria_states = [
   "Ogun"
 ];
 
+
+
 const community_areas = [
   {
     label: "Urban",
@@ -52,11 +54,7 @@ const ranges = [
   [26, 30],
   [31, 35],
   [36, 40],
-  [41, 45],
-  [46, 50],
-  [51, 55],
-  [56, 60],
-  [61, 65],
+  [41, 45]
 ];
 
 const levels_of_education = [
